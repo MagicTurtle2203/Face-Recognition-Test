7135dc1347ca82b08cbbf07b8dc3c2905f43389b
MagicTurtle <raffertychen@gmail.com>
2018-05-19 01:27:34 -0700

    Update train.py and predict.py

--- predict.py
+++ predict.py
@@ -6,39 +6,39 @@ subjects = ["Jiu", "Handong", "Gahyeon", "Yoohyeon"]
 
 
 def draw_rectangle(img, rect):
-	"""Function to draw rectangle on image according to given (x, y) coordinates and given width and height"""
-	(x, y, w, h) = rect
-	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    """Function to draw rectangle on image according to given (x, y) coordinates and given width and height"""
+    (x, y, w, h) = rect
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 
-def draw_text(img, text, x, y):# .
-	"""Function to draw text on give image starting from passed (x, y) coordinates"""
-	cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_DUPLEX,
+def draw_text(img, text, x, y):  # .
+    """Function to draw text on give image starting from passed (x, y) coordinates"""
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_DUPLEX,
                 1.5, (0, 255, 0), 2)
 
 
 def predict(image):
-	img = image.copy()
-	face, rect = detect_face(img)
-	label, confidence = face_recognizer.predict(face)
-	label_text = subjects[label]
+    img = image.copy()
+    face, rect = detect_face(img)
+    label, confidence = face_recognizer.predict(face)
+    label_text = subjects[label]
 
-	draw_rectangle(img, rect)
-	draw_text(img, label_text, rect[0], rect[1]-5)
+    draw_rectangle(img, rect)
+    draw_text(img, label_text + " {0:.2f}".format(confidence), rect[0], rect[1]-5)
 
-	return img
+    return img
 
 
 for file in os.listdir("predictions"):
-	os.remove("predictions/" + file)
+    os.remove("predictions/" + file)
 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-face_recognizer.read("trainer.yml")
+face_recognizer.read("lbph_trainer.yml")
 
 print("Predicting images...")
 for num, image in enumerate(os.listdir("test-data")):
-	print("working on " + image)
-	test_image = cv2.imread("test-data/" + image)
-	predicted_image = predict(test_image)
-	cv2.imwrite("predictions/" + image, predicted_image)
+    print("working on " + image)
+    test_image = cv2.imread("test-data/" + image)
+    predicted_image = predict(test_image)
+    cv2.imwrite("predictions/" + image, predicted_image)
 print("Prediction complete!")
--- train.py
+++ train.py
@@ -1,64 +1,72 @@
 import os
-import shutil
+import cv2
 import numpy as np
 
 
 def detect_face(image):
-	"""returns a cutout of the subjects face as well as the rectangle around the subject"""
-	gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	face_cascade = cv2.CascadeClassifier("face-cascades/lbpcascade_frontalface.xml")
-	faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.2, minNeighbors=5)
+    """returns a cutout of the subjects face as well as the rectangle around the subject"""
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier("face-cascades/lbpcascade_frontalface_improved.xml")
+    faces = face_cascade.detectMultiScale(
+        gray_img, scaleFactor=1.2, minNeighbors=5)
 
-	if (len(faces) == 0):
-		return None, None
+    if len(faces) == 0:
+        return None, None
 
-	(x, y, w, h) = faces[0]
+    (x, y, w, h) = faces[0]
 
-	return gray_img[y:y+h, x:x+w], faces[0]
+    return gray_img[y:y+h, x:x+w], faces[0]
 
 
 def prepare_training_data(data_path):
-	"""Returns a list of faces and their labels for training"""
-    # gets a list of each folder containing the subjects' images
-	dirs = os.listdir(data_path)
-	faces = []
-	labels = []
+    """Returns a list of faces and their labels for training"""
+# gets a list of each folder containing the subjects' images
+    dirs = os.listdir(data_path)
+    faces = []
+    labels = []
 
-	for dir_name in dirs:
-		if not dir_name.startswith('s'):
-			continue
-        # creates a label for each subject by removing the 's' from the directory name and converting to an int
-		label = int(dir_name.replace('s', ''))
-		subject_path = data_path + '/' + dir_name
-		subject_images = os.listdir(subject_path)
+    for dir_name in dirs:
+        if not dir_name.startswith('s'):
+            continue
+    # creates a label for each subject by removing the 's' from the directory name and converting to an int
+        label = int(dir_name.replace('s', ''))
+        subject_path = data_path + '/' + dir_name
+        subject_images = os.listdir(subject_path)
 
-		for image_name in subject_images:
-			print("working on " + subject_path + '/' + image_name)
-			image_path = subject_path + '/' + image_name
-			image = cv2.imread(image_path)
-			#cv2.imshow("Training...", image)
-			#cv2.waitKey(100)
+        for image_name in subject_images:
+            print("working on " + subject_path + '/' + image_name)
+            image_path = subject_path + '/' + image_name
+            image = cv2.imread(image_path)
+            #cv2.imshow("Training...", image)
+            # cv2.waitKey(100)
 
-			face, rect = detect_face(image)
+            face, rect = detect_face(image)
 
-			if face is not None:
-				faces.append(face)
-				labels.append(label)
+            if face is not None:
+                faces.append(face)
+                labels.append(label)
 
-	cv2.destroyAllWindows()
-	cv2.waitKey(1)
-	cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
 
-	return faces, labels
+    return faces, labels
+
 
 if __name__ == "__main__":
-	print("Preparing data...")
-	faces, labels = prepare_training_data("training-data")
-	print("Data prepared")
+    print("Preparing data...")
+    faces, labels = prepare_training_data("training-data")
+    print("Data prepared")
 
-	print("Total faces: {}".format(len(faces)))
-	print("Total labels: {}".format(len(labels)))
+    print("Total faces: {}".format(len(faces)))
+    print("Total labels: {}".format(len(labels)))
 
-	face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-	face_recognizer.train(faces, np.array(labels))
-	face_recognizer.save('trainer.yml')
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    #face_recognizer.read("trainer.yml")
+    #face_recognizer.update(faces, np.array(labels))
+    #face_recognizer.save("trainer.yml")
+
+    face_recognizer.train(faces, np.array(labels))
+    face_recognizer.save("lbph_trainer.yml")
+    print("File saved.")
